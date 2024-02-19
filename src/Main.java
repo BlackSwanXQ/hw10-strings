a35f579d1c4d26caf3549e1a4221c82449f91ef7
Алексей Шлапаков <theblack2swan@gmail.com>
2024-02-19 14:07:25 +0300

    hw10 strings - готово

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-
+//task1
+        System.out.println("task1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = String.join(" ", lastName, firstName, middleName);
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+//task2
+        System.out.println("task2");
+        System.out.println("Ф.И.О. сотрудника для заполнения отчёта - " + fullName.toUpperCase());
+//task3
+        System.out.println("task3");
+        fullName = "Иванов Семён Семёнович";
+        System.out.println(fullName);
+        System.out.println("Ф.И.О. сотрудника - " + fullName.replace('ё', 'е'));
     }
 }
